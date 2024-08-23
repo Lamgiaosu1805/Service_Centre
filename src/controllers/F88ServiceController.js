a67f8956aaf43725e4e84c8c316e85c297bbded3
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-08-23 14:10:25 +0700

    cron job push POL

--- src/controllers/F88ServiceController.js
+++ src/controllers/F88ServiceController.js
@@ -4,6 +4,91 @@ const { SuccessResponse, FailureResponse } = require("../utils/ResponseRequest")
 const { default: mongoose } = require("mongoose")
 const FormPushF88Model = require("../models/FormPushF88Model")
 
+const pushDocument = async (isApi, res, numberCustomer) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const requestId = new Date().getTime().toString()
+        const data = await CustomerModel.aggregate([
+            {$match: {is_active: false}},
+            {$limit: numberCustomer},
+            {
+                $lookup: {
+                    from: 'identitycustomers',
+                    localField: 'phone_number',
+                    foreignField: 'phone_number',
+                    as: 'identities'
+                }
+            },
+            { $unwind: '$identities' },
+        ]).session(session)
+        const dataPush = data.map((item) => {
+            return {
+                CampaignId: 2,
+                SourceId: 393,
+                AssetTypeId: 17,
+                PhoneNumber: item.phone_number,
+                TrackingId: "VNFITE_F88",
+                FullName: item.full_name,
+                Address: item.identities.address
+            }
+        })
+        const response = await axios.post(process.env.F88_API, {
+            PartnerCode: "VNFITE",
+            RequestId: requestId,
+            Data: dataPush
+        });
+        console.log("F88 Response-----------------------------")
+        console.log(response.data)
+        if(response.data.ErrorCode == "200") {
+            const customerIds = data.map(doc => doc._id);
+            const now = new Date(); // Lấy ngày giờ hiện tại
+            const day = String(now.getDate()).padStart(2, '0'); // Lấy ngày và đảm bảo có 2 chữ số
+            const month = String(now.getMonth() + 1).padStart(2, '0'); // Lấy tháng (0-11) và chuyển về 1-12
+            const year = now.getFullYear(); // Lấy năm
+            const listForm = data.map((item) => {
+                return {
+                    id_customer: item._id,
+                    date: `${day}/${month}/${year}`,
+                }
+            })
+            console.log(listForm)
+            await CustomerModel.updateMany({ _id: { $in: customerIds } }, { $set: { is_active: true }}, {session});
+            await FormPushF88Model.insertMany(listForm, {session})
+            isApi == true
+            ?
+            res.json(SuccessResponse({
+                request_id: requestId,
+                message: "Đẩy đơn thành công"
+            }))
+            :
+            console.log(SuccessResponse({
+                request_id: requestId,
+                message: "Đẩy đơn thành công"
+            }))
+        } else {
+            isApi == true
+            ?
+            res.json(FailureResponse("03", {
+                data: response.data,
+                request_id: requestId,
+            }))
+            :
+            console.log(FailureResponse("03", {
+                data: response.data,
+                request_id: requestId,
+            }))
+        }
+        console.log("-----------------------------")
+        await session.commitTransaction();
+        session.endSession();
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        res.json(FailureResponse("04", error))
+    }
+} 
+
 const F88ServiceController = {
     pushDocumentRequest: async (req, res) => {
         const body = req.body
@@ -71,74 +156,8 @@ const F88ServiceController = {
         }
     },
     pushData: async(req, res) => {
-        const session = await mongoose.startSession();
-        session.startTransaction();
-        try {
-            const requestId = new Date().getTime().toString()
-            const data = await CustomerModel.aggregate([
-                {$match: {is_active: false}},
-                {$limit: 30},
-                {
-                    $lookup: {
-                        from: 'identitycustomers',
-                        localField: 'phone_number',
-                        foreignField: 'phone_number',
-                        as: 'identities'
-                    }
-                },
-                { $unwind: '$identities' },
-            ]).session(session)
-            const dataPush = data.map((item) => {
-                return {
-                    CampaignId: 2,
-                    SourceId: 393,
-                    AssetTypeId: 17,
-                    PhoneNumber: item.phone_number,
-                    TrackingId: "VNFITE_F88",
-                    FullName: item.full_name,
-                    Address: item.identities.address
-                }
-            })
-            const response = await axios.post(process.env.F88_API, {
-                PartnerCode: "VNFITE",
-                RequestId: requestId,
-                Data: dataPush
-            });
-            console.log("F88 Response ")
-            console.log(response.data)
-            if(response.data.ErrorCode == "200") {
-                const customerIds = data.map(doc => doc._id);
-                const now = new Date(); // Lấy ngày giờ hiện tại
-                const day = String(now.getDate()).padStart(2, '0'); // Lấy ngày và đảm bảo có 2 chữ số
-                const month = String(now.getMonth() + 1).padStart(2, '0'); // Lấy tháng (0-11) và chuyển về 1-12
-                const year = now.getFullYear(); // Lấy năm
-                const listForm = data.map((item) => {
-                    return {
-                        id_customer: item._id,
-                        date: `${day}/${month}/${year}`,
-                    }
-                })
-                await CustomerModel.updateMany({ _id: { $in: customerIds } }, { $set: { is_active: true }}, {session});
-                await FormPushF88Model.insertMany(listForm, {session})
-                res.json(SuccessResponse({
-                    request_id: requestId,
-                    message: "Đẩy đơn thành công"
-                }))
-            } else {
-                res.json(FailureResponse("03", {
-                    data: response.data,
-                    request_id: requestId,
-                }))
-            }
-            await session.commitTransaction();
-            session.endSession();
-        } catch (error) {
-            await session.abortTransaction();
-            session.endSession();
-            res.json(FailureResponse("04", error))
-        }
-        
+        await pushDocument(true, res, 30)
     }
 }
 
-module.exports = F88ServiceController
+module.exports = {F88ServiceController, pushDocument}
