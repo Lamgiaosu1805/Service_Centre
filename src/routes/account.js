0656af2c705e490737422e486944dde2465e7b7c
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-12-03 15:50:50 +0700

    add api admin

--- src/routes/account.js
+++ src/routes/account.js
@@ -2,7 +2,7 @@ const express = require('express');
 const AccountController = require('../controllers/AccountController');
 const router = express.Router()
 
-// router.post('/create-admin', AccountController.createAccount);
+router.post('/create-admin', AccountController.createAccount);
 router.post('/signIn', AccountController.signIn);
 
 module.exports = router;
