a64546d03ee68b91acd3efb2b8a2f84de1a19b5b
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-12-12 15:42:39 +0700

    update

--- src/models/FormPushF88Model.js
+++ src/models/FormPushF88Model.js
@@ -7,7 +7,7 @@ const FormPushF88 = new Schema({
     date: {type: String, required: true},
      // result_push: 1, Thành côg; 0, thất bại; #
     result_push: { type: Number, default: -1 },
-    //status 1- Đang chăm sóc; 2- Thành công; 3- Hủy; 4: Đã chấp thuận phía VNFITE; -1: Chưa chăm sóc; 0: Chưa quyết định, chăm sóc lại sau; Khác: Không xác định
+    //status 1- Đang chăm sóc; 2- Thành công; 3- Hủy; 4: Đã chấp thuận phía VNFITE; -1: Chưa chăm sóc; 0: Chưa quyết định, chăm sóc lại sau; Khác: Không xác định; 5: Khách hàng đã có khoản vay ở F88
     status: { type: Number, default: -1 },
     price_debit: { type: String, default: "" },
     insurance: {type: String, default: ""},
